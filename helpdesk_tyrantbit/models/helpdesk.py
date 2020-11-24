287766c489acffc7fbba770e3f93f445b73baee0
Juanma Tyrantbit <tyrantbit@gmail.com>
2020-11-24 18:48:09 +0100

    UPD added views and new fields

--- helpdesk_tyrantbit/models/helpdesk.py
+++ helpdesk_tyrantbit/models/helpdesk.py
@@ -5,6 +5,23 @@ class HelpdeskTicket(models.Model):
     _name = 'helpdesk.ticket'
     _description = 'Helpdesk Ticket'
 
-    name = fields.Char(string='Name')
+    name = fields.Char(string='Name', required=True)
     description = fields.Text(string='Description')
     date = fields.Date(string='Date')
+    state = fields.Selection(selection=[
+        ('new', 'New'),
+        ('assigned', 'Assigned'),
+        ('progress', 'Progress'),
+        ('waiting', 'Waiting'),
+        ('done', 'Done'),
+        ('cancel', 'Cancel'),
+    ], string='State', default='new')
+    dedicated_time = fields.Float(string='Time')
+    assigned = fields.Boolean(string='Assigned', readonly=True)
+    date_due = fields.Date(string='Date Due')
+    corrective_action = fields.Html(
+        help='Detail of corrective action after this issue'
+    )
+    preventive_action = fields.Html(
+        help='Detail of preventive action after this issue'
+    )
