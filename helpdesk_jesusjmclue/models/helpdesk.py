0856eae8c06a11268383fb1ea46bb914f7a2b717
Jesús Jorge Muñoz <jesus@clue.aero>
2020-11-24 18:03:55 +0000

    ADD Model fields, tree and form views

--- helpdesk_jesusjmclue/models/helpdesk.py
+++ helpdesk_jesusjmclue/models/helpdesk.py
@@ -5,8 +5,35 @@ class HelpdeskTicket(models.Model):
 	_description = "Helpdesk Ticket"
 
 	name = fields.Char(
-		string = 'Name')
+		string = 'Name',
+		required = True)
 	description = fields.Text(
 		string = 'Description')
 	date = fields.Date(
 		string = 'Date')
+
+	state = fields.Selection(
+		[('new', 'New'),
+		 ('assigned', 'Assigned'),
+		 ('progress', 'Progress'),
+		 ('waiting', 'Waiting'),
+		 ('done', 'Done'),
+		 ('cancel', 'Cancel')],
+		string = 'State',
+		default = 'new')
+
+	dedicated_time = fields.Float(
+		string = 'Time')
+
+	assigned = fields.Boolean(
+		string = 'Assigned',
+		readonly = True)
+
+	due_date = fields.Date(
+		string = 'Due Date')
+
+	corrective_action = fields.Html(
+		help = 'Detail of corrective action after this issue')
+
+	preventive_action = fields.Html(
+		help = 'Detail of preventive action after this issue')
