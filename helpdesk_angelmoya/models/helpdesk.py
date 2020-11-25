c1fb68547e5bc7f9702efca11c73058cc9872e2a
angelmoya <amoyapardo@gmail.com>
2020-11-25 08:41:40 +0100

    WIP

--- helpdesk_angelmoya/models/helpdesk.py
+++ helpdesk_angelmoya/models/helpdesk.py
@@ -6,8 +6,33 @@ class HelpdeskTicket(models.Model):
     _description = "Helpesk Ticket"
 
     name = fields.Char(
-        string='Name')
+        string='Name',
+        required=True)
     description = fields.Text(
         string='Description')
     date = fields.Date(
         string='Date')
+    state = fields.Selection(
+        [('new', 'New'),
+         ('assigned', 'Assigned'),
+         ('progress', 'Progress'),
+         ('waiting', 'Waiting'),
+         ('done', 'Done'),
+         ('cancel', 'Cancel')],
+        string='State',
+        default='new')
+    dedicated_time = fields.Float(
+        string='Time')
+
+    assigned = fields.Boolean(
+        string='Assigned',
+        readonly=False)
+
+    date_due = fields.Date(
+        string='Date Due')
+
+    corrective_action = fields.Html(
+        help='Detail of corrective action after this issue'
+    )
+    preventive_action = fields.Html(
+        help='Detail of preventive action after this issue')
