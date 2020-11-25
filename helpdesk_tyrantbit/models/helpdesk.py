2be3bf6703013cfeb7dce6465647323c880a8871
Juanma Tyrantbit <tyrantbit@gmail.com>
2020-11-25 18:01:01 +0100

    [ADD] Module Helpdesk Tyrantbit

--- helpdesk_tyrantbit/models/helpdesk.py
+++ helpdesk_tyrantbit/models/helpdesk.py
@@ -15,13 +15,38 @@ class HelpdeskTicket(models.Model):
         ('waiting', 'Waiting'),
         ('done', 'Done'),
         ('cancel', 'Cancel'),
-    ], string='State', default='new')
+    ],
+                             string='State',
+                             default='new')
     dedicated_time = fields.Float(string='Time')
     assigned = fields.Boolean(string='Assigned', readonly=True)
     date_due = fields.Date(string='Date Due')
     corrective_action = fields.Html(
-        help='Detail of corrective action after this issue'
-    )
+        help='Detail of corrective action after this issue')
     preventive_action = fields.Html(
-        help='Detail of preventive action after this issue'
-    )
+        help='Detail of preventive action after this issue')
+    user_id = fields.Many2one(comodel_name='res.users', string='Assigned to')
+
+    def set_assigned(self):
+        self.ensure_one()
+        self.write({
+            'assigned': True,
+            'state': 'assigned',
+            'user_id': self.env.uid
+        })
+
+    def set_progress(self):
+        self.ensure_one()
+        self.state = 'progress'
+
+    def set_waiting(self):
+        self.ensure_one()
+        self.state = 'waiting'
+
+    def set_done(self):
+        self.ensure_one()
+        self.state = 'done'
+
+    def set_cancel(self):
+        self.ensure_one()
+        self.state = 'cancel'
