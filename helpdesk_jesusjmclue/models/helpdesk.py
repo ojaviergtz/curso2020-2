76717db64178062f25b3af18c9acf246c2ea8076
Jesús Jorge Muñoz <jesus@clue.aero>
2020-11-25 18:14:13 +0000

    [IMP] Add state buttons with their methods

--- helpdesk_jesusjmclue/models/helpdesk.py
+++ helpdesk_jesusjmclue/models/helpdesk.py
@@ -15,10 +15,10 @@ class HelpdeskTicket(models.Model):
 	state = fields.Selection(
 		[('new', 'New'),
 		 ('assigned', 'Assigned'),
-		 ('progress', 'Progress'),
+		 ('progress', 'In Progress'),
 		 ('waiting', 'Waiting'),
 		 ('done', 'Done'),
-		 ('cancel', 'Cancel')],
+		 ('cancelled', 'Cancelled')],
 		string = 'State',
 		default = 'new')
 
@@ -37,3 +37,32 @@ class HelpdeskTicket(models.Model):
 
 	preventive_action = fields.Html(
 		help = 'Detail of preventive action after this issue')
+
+	def set_assigned_multi(self):
+		for ticket in self:
+			ticket.set_assigned()
+
+	def set_assigned(self):
+		self.ensure_one()
+		self.write({
+			'assigned' : True,
+			'state' : 'assigned',
+		})
+
+	def set_progress(self):
+		self.ensure_one()
+		self.state = 'progress'
+
+	def set_waiting(self):
+		self.ensure_one()
+		self.state = 'waiting'
+
+	def set_done(self):
+		self.ensure_one()
+		self.state = 'done'
+
+	def set_cancel(self):
+		self.ensure_one()
+		self.state = 'cancelled'
+
+	
