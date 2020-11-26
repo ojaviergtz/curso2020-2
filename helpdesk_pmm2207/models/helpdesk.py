e01fa8e90fd9c646f70700dbc22665475f779353
pmm2207 <pmm2207@gmail.com>
2020-11-26 10:23:26 +0100

    Commit test

---
+++ helpdesk_pmm2207/models/helpdesk.py
@@ -0,0 +1,13 @@
+from odoo import models, fields
+
+
+class HelpdeskTicket(models.Model):
+    _name = 'helpdesk.ticket'
+    _description = "Helpdesk Ticket"
+
+    name = fields.Char(
+        string='Name')
+    description = fields.Text(
+        string='Description')
+    date = fields.Date(
+        string='Date')
