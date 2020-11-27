33fee82b33ea7fbec3d5e1e6296b9850c8d1fe9f
Osiris <elec.osiris.2.work@gmail.com>
2020-11-27 17:14:21 +0000

    Task 2. Added: tree & form views to helpdesk module

--- helpdesk_osirisgtz/models/helpdesk.py
+++ helpdesk_osirisgtz/models/helpdesk.py
@@ -7,7 +7,31 @@ class HelpdeskTicket (models.Model):
     _description = "Helpdesk Ticket"
 
     name = fields.Char(
-            'Name')
+            'Name',
+            required=True)
     description = fields.Text(
             'Description')
     date = fields.Date( 'Date' )
+
+    state = fields.Selection(
+            [('new','New'),
+             ('assigned','Assigned'),
+             ('in_progress', 'In Progress'),
+             ('pendign', 'Pending'),
+             ('done', 'Done'),
+             ('cancel','Cancel')],
+            default = 'new')
+
+    dedicated_time = fields.Float('Time')
+
+    assigned = fields.Boolean(
+            readonly= True )
+
+    due_date = fields.Date('Due Date')
+
+    corrective_action = fields.Html(
+            help = 'To add all actions taken to fix the issue'
+            )
+    preventive_action = fields.Html(
+            help = 'To add actions to prevent the issue to happen')
+
