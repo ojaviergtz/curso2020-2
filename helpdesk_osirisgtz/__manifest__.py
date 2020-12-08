a30fa5dc1c0cdcf0ac45d51710b10f61a5354cb0
Osiris <elec.osiris.2.work@gmail.com>
2020-12-08 15:55:25 +0000

    - crear un o2m en helpdesk.ticket apuntando a un nuevo modelo helpdesk.ticket.action que tenga un campo name y un campo date.  - crear un campo helpdesk.tag y añadir un m2m de etiquetas en el ticket.  - añadir un m2m en helpdesk.tag para ver todas las tareas relacionadas con cada tag.  - Hacer que el campo assigned sea calculado.  - hacer un campo calculado que indique, dentro de un ticket, la cantidad de tiquets asociados al mismo ususario.  - crear un campo nombre de etiqueta, y hacer un botón que cree la nueva etiqueta con ese nombre y lo asocie al ticket.

--- helpdesk_osirisgtz/__manifest__.py
+++ helpdesk_osirisgtz/__manifest__.py
@@ -21,6 +21,7 @@
         "security/helpdesk_security.xml",
         "security/ir.model.access.csv",
         "views/helpdesk_ticket_views.xml",
+        "views/helpdesk_tag_views.xml",
     ],
     "demo": [
     ]
--- helpdesk_osirisgtz/models/helpdesk.py
+++ helpdesk_osirisgtz/models/helpdesk.py
@@ -1,4 +1,30 @@
-from odoo import models, fields
+from odoo import models, fields, api
+
+
+class HelpdeskTicketAction(models.Model):
+    """  """
+    _name= 'helpdesk.ticket.action'
+    _description= " Model to define all Actions in tickets"
+
+    name = fields.Char(
+            'Name')
+    date = fields.Date(
+            'Date')
+    ticket_id = fields.Many2one(
+            comodel_name='helpdesk.ticket')
+
+class HelpdeskTags(models.Model):
+    """   """
+    _name = 'helpdesk.tag'
+    _description = " Model to define tags to helpdesk tickets"
+
+    name = fields.Char('Name')
+    ticket_ids = fields.Many2many(
+            comodel_name= 'helpdesk.ticket',
+            relation= 'helpdesk_ticket_tag_rel',
+            column1= 'tag_id',
+            column2= 'ticket_id',
+            string="Tickets")
 
 
 class HelpdeskTicket (models.Model):
@@ -25,7 +51,7 @@ class HelpdeskTicket (models.Model):
     dedicated_time = fields.Float('Time')
 
     assigned = fields.Boolean(
-            readonly= True )
+            compute="_compute_assigned")
 
     due_date = fields.Date('Due Date')
 
@@ -39,13 +65,59 @@ class HelpdeskTicket (models.Model):
             comodel_name = 'res.users',
             string = 'Assigned to')
 
+    action_ids = fields.One2many(
+            comodel_name='helpdesk.ticket.action',
+            inverse_name='ticket_id',
+            string='Actions')
+    tag_ids = fields.Many2many(
+            comodel_name= 'helpdesk.tag',
+            relation= 'helpdesk_ticket_tag_rel',
+            column1='ticket_id',
+            column2= 'tag_id',
+            string='Tags')
+
+    tickets_for_user = fields.Integer(
+            string='Tickets for user assigned',
+            compute="_compute_tickets_for_user")
+
+    tag_generator = fields.Char(
+            string= 'Add new Tag')
+
+
     # Methods
+    def create_new_tag(self):
+        """   """
+        self.ensure_one()
+        if self.tag_generator:
+            new_tag = self.env['helpdesk.tag'].create({
+                'name': self.tag_generator,
+                #'ticket_ids':[(4,self.id,0)]
+                })
+            #self.write({'tag_ids': [(4,new_tag.id,0)]})
+            self.tag_ids += new_tag # This works in this odoo version
+
+    @api.depends('user_id')
+    def _compute_tickets_for_user(self):
+        """  """
+        for ticket in self:
+            user = ticket.user_id
+            all_tickets = self.env['helpdesk.ticket'].search([
+                ('user_id', '=', user.id)
+                ])
+            ticket.tickets_for_user = len(all_tickets)
+
+    @api.depends('user_id')
+    def _compute_assigned(self):
+        """  """
+        for ticket in self:
+            ticket.assigned = ticket.user_id and True
+
     def btn_assigned(self):
         """"""
         self.ensure_one()
         self.write({
             'state':'assigned',
-            'assigned': True,
+            #'assigned': True,
             'user_id': self.env.user.id
             })
 
